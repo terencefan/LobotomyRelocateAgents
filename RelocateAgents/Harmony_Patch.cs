04e448d7dfca6791f42ece874d1ddb2b2b75aae9
Terence Fan <tefa@microsoft.com>
2022-02-02 03:53:10 +0800

    relocate agents

---
+++ RelocateAgents/Harmony_Patch.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reflection;
+
+using AutoInority.Extentions;
+
+using Harmony;
+
+using UnityEngine;
+
+namespace TodayOrdeal
+{
+    public class Harmony_Patch
+    {
+        public const string ModName = "Lobotomy.inority.RelocateAgents";
+
+        public Harmony_Patch()
+        {
+            Invoke(() =>
+            {
+                HarmonyInstance mod = HarmonyInstance.Create(ModName);
+                PatchUnitMouseEventManager(mod);
+                PatchSefiraRecoverGaugeUI(mod);
+            });
+        }
+
+        public static bool ClickSefiraReturn_Prefix(SefiraRecoverGaugeUI __instance)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                Invoke(() => SefiraManager.instance.sefiraList.ForEach(x => x.MoveToSefira(__instance.sefiraEnum)));
+                return false;
+            }
+            return true;
+        }
+
+        public static void UnitMouseEventManager_Update_Postfix(UnitMouseEventManager __instance)
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                {
+                    Invoke(() => SefiraManager.instance.sefiraList.ForEach(x => x.MoveToNeighborPassage()));
+                    return;
+                }
+                Invoke(() => SefiraManager.instance.sefiraList.ForEach(x => x.ReturnAgentsToSefira()));
+            }
+        }
+
+        public void PatchSefiraRecoverGaugeUI(HarmonyInstance mod)
+        {
+            var prefix = typeof(Harmony_Patch).GetMethod(nameof(ClickSefiraReturn_Prefix));
+            var origin = typeof(SefiraRecoverGaugeUI).GetMethod(nameof(SefiraRecoverGaugeUI.OnClickSefiraReturn));
+            mod.Patch(origin, new HarmonyMethod(prefix), null);
+        }
+
+        public void PatchUnitMouseEventManager(HarmonyInstance mod)
+        {
+            var postfix = typeof(Harmony_Patch).GetMethod(nameof(UnitMouseEventManager_Update_Postfix));
+            var origin = typeof(UnitMouseEventManager).GetMethod("Update", BindingFlags.NonPublic | BindingFlags.Instance);
+            mod.Patch(origin, null, new HarmonyMethod(postfix));
+        }
+
+        private static void Invoke(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
+        }
+    }
+}
---
+++ RelocateAgents/SefiraExtension.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using TodayOrdeal;
+
+namespace AutoInority.Extentions
+{
+    public static class SefiraExtensions
+    {
+        private static readonly Dictionary<SefiraEnum, List<SefiraEnum>> _neighborDict = new Dictionary<SefiraEnum, List<SefiraEnum>>()
+        {
+            { SefiraEnum.MALKUT, new List<SefiraEnum>(){SefiraEnum.YESOD, SefiraEnum.NETZACH, SefiraEnum.HOD} },
+            { SefiraEnum.YESOD, new List<SefiraEnum>(){SefiraEnum.MALKUT, SefiraEnum.NETZACH, SefiraEnum.HOD} },
+            { SefiraEnum.NETZACH, new List<SefiraEnum>(){SefiraEnum.YESOD, SefiraEnum.MALKUT} },
+            { SefiraEnum.HOD, new List<SefiraEnum>(){SefiraEnum.YESOD, SefiraEnum.MALKUT} },
+            { SefiraEnum.TIPERERTH1, new List<SefiraEnum>(){SefiraEnum.GEBURAH, SefiraEnum.CHESED} },
+            { SefiraEnum.TIPERERTH2, new List<SefiraEnum>(){SefiraEnum.GEBURAH, SefiraEnum.CHESED} },
+            { SefiraEnum.GEBURAH, new List<SefiraEnum>(){SefiraEnum.TIPERERTH1, SefiraEnum.TIPERERTH2, SefiraEnum.BINAH} },
+            { SefiraEnum.CHESED, new List<SefiraEnum>(){SefiraEnum.TIPERERTH1, SefiraEnum.TIPERERTH2, SefiraEnum.CHOKHMAH} },
+            { SefiraEnum.CHOKHMAH, new List<SefiraEnum>(){SefiraEnum.CHESED} },
+            { SefiraEnum.BINAH, new List<SefiraEnum>(){SefiraEnum.GEBURAH} },
+        };
+
+        public static IEnumerable<SefiraEnum> GetNeighborEnums(SefiraEnum sefiraEnum)
+        {
+            if (_neighborDict.TryGetValue(sefiraEnum, out var sefiraEnums))
+            {
+                return sefiraEnums;
+            }
+            return new SefiraEnum[] { };
+        }
+
+        public static int GetPriority(this Sefira sefira)
+        {
+            switch ((SefiraEnum)sefira.index)
+            {
+                case SefiraEnum.TIPERERTH1:
+                case SefiraEnum.TIPERERTH2:
+                    return 10;
+                case SefiraEnum.CHOKHMAH:
+                case SefiraEnum.BINAH:
+                    return 20;
+                case SefiraEnum.CHESED:
+                case SefiraEnum.GEBURAH:
+                    return 30;
+                case SefiraEnum.NETZACH:
+                case SefiraEnum.HOD:
+                    return 40;
+                case SefiraEnum.MALKUT:
+                case SefiraEnum.YESOD:
+                default:
+                    return 50;
+            }
+        }
+
+        public static void MoveToNeighborPassage(this Sefira sefira)
+        {
+            var passage = sefira.passageList.Where(x => x.isActivate && x.type == PassageType.HORIZONTAL);
+            if (passage.Any())
+            {
+                sefira.agentList.ForEach(x => x.SetWaitingPassage(passage.First()));
+            }
+        }
+
+        public static void MoveToNetzachElevator(this Sefira sefira)
+        {
+            var passages = SefiraManager.instance.GetSefira(SefiraEnum.YESOD).passageList;
+            var elevator = passages.Where(x => x.isActivate && x.GetSrc() == "Map/Passage/Yesod/PassageYesodElevator" && x.passageGroup == "1").First();
+            sefira.agentList.ForEach(x => x.SetWaitingPassage(elevator));
+        }
+
+        public static void MoveToSefira(this Sefira sefira, SefiraEnum sefiraId)
+        {
+            var target = SefiraManager.instance.GetSefira(sefiraId);
+            MapNode sepiraNodeByRandom = MapGraph.instance.GetSepiraNodeByRandom(target.indexString);
+            if (sepiraNodeByRandom == null)
+            {
+                return;
+            }
+            var passage = sepiraNodeByRandom.GetAttachedPassage();
+            sefira.agentList.ForEach(x => x.SetWaitingPassage(passage));
+        }
+    }
+}
